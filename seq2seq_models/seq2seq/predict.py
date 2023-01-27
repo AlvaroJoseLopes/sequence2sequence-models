da9e4f627f3c631328f4c07db300240430ed1f54
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2023-01-26 21:04:58 -0300

    add arguments to predict method

--- seq2seq_models/seq2seq/predict.py
+++ seq2seq_models/seq2seq/predict.py
@@ -2,39 +2,50 @@ import argparse
 import torch
 from torchtext.data.utils import get_tokenizer
 
-def predict(*args):
-    args = parse_args(*args)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    model = torch.load(args.file).to(device)
+def predict(model, examples, src_lang, trg_lang, device):
     src_tokenizer = get_tokenizer('spacy', language=args.src_lang)
     src_vocab = torch.load(f'vocabs/vocab_{args.src_lang}.pth').to(device)
     trg_vocab = torch.load(f'vocabs/vocab_{args.trg_lang}.pth').to(device)
 
     _tokenize = lambda sentence, tokenizer, vocab: \
             [vocab[token] for token in tokenizer(sentence)]
-    example = _tokenize(args.example, src_tokenizer, src_vocab)
-    example = torch.tensor([[src_vocab['<bos>']] + example + [src_vocab['<eos>']]], dtype=torch.int64, device=device)
-    example = example.reshape(example.shape[-1], 1)
+    examples = [
+            torch.tensor([src_vocab['<bos>']] 
+            + _tokenize(example, src_tokenizer, src_vocab) 
+            + [src_vocab['<eos>']], dtype=torch.int64, device=device)
+        for example in examples
+    ]
+    examples = pad_sequence(examples, padding_value=src_vocab['<pad>'])
+    examples = examples.clone().detach()
 
-    preds = model.predict(example)
-    preds = [int(token) for token in list(preds)][1:]
-    preds = trg_vocab.lookup_tokens(preds)
-    preds = preds[:preds.index('<eos>')]
-    print(' '.join(preds))
+    preds = model.predict(examples)
+    preds = torch.transpose(preds, 0, 1)
+    # print(preds.shape)
+    # preds = [int(token) for token in list(preds)][1:]
+    # preds = trg_vocab.lookup_tokens(preds)
+    # preds = preds[:preds.index('<eos>')]
+    # print(' '.join(preds))
+    preds = [trg_vocab.lookup_tokens([int(token) for token in pred]) for pred in preds.tolist()]
+    return [' '.join(pred[1:pred.index('<eos>')]) for pred in preds]
 
 def parse_args(*args):
     parser = argparse.ArgumentParser(description='Seq2Seq model predict sample script')
     parser.add_argument('-src', '--src_lang', type=str, help='source language', required=True)
     parser.add_argument('-trg', '--trg_lang', type=str, help='target language', required=True)
-    parser.add_argument('-f', '--file', type=str, help='file of the model (.pt)')
-    parser.add_argument('-ex', '--example', type=str, help="sentence example (len < 30) to predict")    
+    parser.add_argument('-fm', '--file_model', type=str, help='file of the model (.pt)')
+    parser.add_argument('-fe', '--file_examples', type=str, help='file of the examples (.txt)')    
     return parser.parse_args(*args)
 
 if __name__ == '__main__':
     from data import *
     from model import Seq2Seq
-    predict()
+    args = parse_args()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = torch.load(args.file_model).to(device)
+    f = open(args.file_examples)
+    examples = f.read().splitlines()
+    print(examples)
+    print(predict(model, examples, args.src_lang, args.trg_lang, device))
 else:
     from .data import *
     from .model import Seq2Seq
